0a910c2e2c9391d629cf3c87964f2f34a65da16d
SergioAle210 <ore221122@uvg.edu.gt>
2024-05-14 23:41:37 -0600

    feat: Enhanced ESLint configuration

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types'
 import './Display.css'
 
 const Display = ({ value }) => {
-    let displayValue = value;
+    let displayValue = value
 
     // Convertir el valor a número para la comparación
-    const numericValue = Number(value);
+    const numericValue = Number(value)
 
     if (numericValue > 999999999 || value.length > 9) {
-        displayValue = "ERROR";
+        displayValue = "ERROR"
     }
 
     return <div className="display">{displayValue}</div>
